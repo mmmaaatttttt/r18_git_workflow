4df5c5d8cf0fe3367b785c38baae4ce47319926b
Matt Lane <lane.matthew@gmail.com>
2021-01-04 10:31:23 -0800

    edit add function per PR review

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const firstName = "Tim";
 
 console.log(`${firstName}'s secret is ${SECRET}`);
 
-function add(x, y) {
-  return x + y;
+/**
+ * adds two numbers together and returns the sum.
+ *
+ * @param {Number} num1
+ * @param {Number} num2
+ */
+function add(num1, num2) {
+  return num1 + num2;
 }
 
 console.log("goodbye!");
